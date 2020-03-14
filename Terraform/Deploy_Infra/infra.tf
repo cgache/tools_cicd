61efa3ccaf672a5ce40240c079867fd003e07106
Anthony919498 <50576200+Anthony919498@users.noreply.github.com>
2020-03-14 17:44:42 +0100

    Update infra.tf

--- Terraform/Deploy_Infra/infra.tf
+++ Terraform/Deploy_Infra/infra.tf
@@ -103,7 +103,7 @@ resource "aws_subnet" "subnet-private-3" {
 
 # Nat Instance
 resource "aws_instance" "nat" {
-  ami                    = "ami-01bb0dc93a0622b92"
+  ami                    = "ami-02f9d535e6f6070f7"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet-public-1.id
   vpc_security_group_ids = [aws_security_group.allow_nat.id]
