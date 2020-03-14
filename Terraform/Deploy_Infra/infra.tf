d16e2b58423e676bb276ab2ef4f486d73b0a5241
cgache <50575572+cgache@users.noreply.github.com>
2020-03-14 18:53:07 +0100

    Update infra.tf

--- Terraform/Deploy_Infra/infra.tf
+++ Terraform/Deploy_Infra/infra.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "${var.default-aws-region}"
 }
 
 variable "env" {
   type    = string
-  default = "dev"
+  default = "DEV"
 }
+
 terraform {
   backend "s3" {
     }
@@ -39,7 +40,7 @@ resource "aws_subnet" "subnet-public-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "${var.default-aws-region}a"
   tags = {
     Name = "${var.env}-subnet-public-1"
   }
@@ -50,7 +51,7 @@ resource "aws_subnet" "subnet-public-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "${var.default-aws-region}b"
   tags = {
     Name = "${var.env}-subnet-public-2"
   }
@@ -61,7 +62,7 @@ resource "aws_subnet" "subnet-public-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2c"
+  availability_zone       = "${var.default-aws-region}c"
   tags = {
     Name = "${var.env}-subnet-public-3"
   }
@@ -73,7 +74,7 @@ resource "aws_subnet" "subnet-private-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "${var.default-aws-region}a"
   tags = {
     Name = "${var.env}-subnet-private-1"
   }
@@ -84,7 +85,7 @@ resource "aws_subnet" "subnet-private-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "${var.default-aws-region}b"
   tags = {
     Name = "${var.env}-subnet-private-2"
   }
@@ -95,7 +96,7 @@ resource "aws_subnet" "subnet-private-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2c"
+  availability_zone       = "${var.default-aws-region}c"
   tags = {
     Name = "${var.env}-subnet-private-3"
   }
